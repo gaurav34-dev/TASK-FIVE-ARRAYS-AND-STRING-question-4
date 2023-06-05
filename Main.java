bee34ab6df8769a2684c137a2ea76129bd4c85c7
gbaser54 <68541784+gbaser54@users.noreply.github.com>
2023-06-05 08:56:22 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 import java.util.*;
-import java.util.Arrays;
+//import java.util.Arrays;
 public class Main {
 public static void main(String[] args) 
 {
@@ -7,21 +7,21 @@ public static void main(String[] args)
 System.out.print("Enter a string: ");  
 String str= sc.nextLine();  
 char arr[]=str.toCharArray();
-  char temp;
+  //char temp;
   int counter=0;
-  for(int i=0;i<arr.length;i++)
-    {
-      for(int j=i+1;j<arr.length;j++)
-        {
-          if(arr[i]>arr[j])
-          {
-            temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-          }
-        }
-    }
-  System.out.println(arr);
+  //for(int i=0;i<arr.length;i++)
+    //{
+      //for(int j=i+1;j<arr.length;j++)
+        //{
+          //if(arr[i]>arr[j])
+          //{
+            //temp=arr[i];
+            //arr[i]=arr[j];
+            //arr[j]=temp;
+          //}
+        //}
+    //}
+  //System.out.println(arr);
 
   for(int i=0;i<arr.length;i++)
     {
